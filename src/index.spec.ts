c649a0db191d40edd23a6cdb56e4160a484525cc
wgb-code <front.willian@gmail.com>
2025-07-27 21:56:30 -0300

    chore: add express in project

--- src/index.spec.ts
+++
@@ -1,6 +0,0 @@
-import Person from './index';
-
-it('should sum', () => {
-	const person = new Person();
-	expect(person.sayMyName()).toBe('Willian Gabriel');
-});
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-class Person {
-	sayMyName() {
-		return 'Willian Gabriel';
-	}
-}
+import express from 'express';
+import { Request, Response } from 'express';
 
-export default Person;
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+	res.send('Hello, World!');
+});
+
+app.listen(3000, () => {
+	console.log(`Server is running on http://localhost:3000`);
+});
